b136896534e0634a6d13ed0a7bcfb285c71c7aea
carlos <car.l1991@yahoo.com.br>
2023-11-23 14:00:18 -0300

    Atualizando Python

---
+++ Udemy/Python/Exercicios/diagonal_negativos.py
@@ -0,0 +1,26 @@
+n: int
+
+n = int(input("Qual a ordem da matriz? "))
+
+mat = [[0 for x in range(n)] for x in range(n)]
+
+for i in range(0,n):
+    for j in range(0,n):
+        mat[i][j] = int(input(f"ELEMENTO [ {i},{j} ]: "))
+
+print()
+print("Diagonal Principal:")
+for i in range(0,n):
+    print(f"{mat[i][i]}" , end=" ")
+print()
+
+print()
+cont = 0
+for i in range(0,n):
+    for j in range(0,n):
+        if mat[i][j] < 0:
+            cont = cont + 1
+
+
+print(f"Quantidade negativos = {cont}")
+
